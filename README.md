b7a93f2e71183109024912ae439f09d703140ef0
icchy <r.icchy.m4cc3r@gmail.com>
2015-12-16 00:27:35 +0900

    つらい

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # sinchokuwatcher
 進捗無いのにツイッターしてるんですか？ｗ
+
+## Requirements
+ * tweepy
+
+## Usage
+ set twitter auth keys and run
+ `python sinchoku.py report.pdf`
---
+++ sinchoku.py
@@ -0,0 +1,85 @@
+# coding:utf-8
+
+import tweepy
+import os, sys
+import time, datetime
+
+# Twitter auth
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
+
+# watch conf
+INTERVAL = {
+    "write": 5*60, # seconds
+    "sleep": 30*60
+}
+
+
+STATE = {
+    "stop": 0,
+    "write": 1,
+    "sleep": 2
+}
+
+
+def get_auth():
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    return auth
+
+
+def tweet(s, api, me):
+    date = datetime.datetime.now().strftime('%m/%d %H:%M:%S')
+    api.update_status('@{0} {1} [{2}]'.format(me.screen_name, s, date))
+
+
+if __name__ in '__main__':
+    if len(sys.argv) < 2:
+        print >> sys.stderr, " ".join(["usage:", sys.argv[0], "<report.tex>"])
+        sys.exit(1)
+    fname = os.path.join(os.path.dirname(__file__), sys.argv[1])
+    if not os.path.exists(fname):
+        while not os.path.exists(fname):
+            time.sleep(1)
+
+    api = tweepy.API(get_auth())
+    me = api.me()
+    if '-q' not in sys.argv and '--quiet' not in sys.argv:
+        tweet("レポートを書き始めたゾイ: (継続ノルマ:{0}分, 居眠り検知:{1}分)".format(INTERVAL["write"]/60, INTERVAL["sleep"]/60), api, me)
+
+    prev = os.path.getmtime(fname)
+    state = STATE["write"]
+    sleep_cnt = 2
+    while True:
+        try:
+            cur = os.path.getmtime(fname)
+            d = int(time.time()) - prev
+            if d > INTERVAL["sleep"]:
+                if state == STATE["stop"]:
+                    tweet("寝てるゾイ", api, me)
+                    state = STATE["sleep"]
+                elif state == STATE["sleep"] and d > INTERVAL["sleep"]*sleep_cnt:
+                    tweet("まだ寝てるんですか？ｗ", api, me)
+                    sleep_cnt += 1
+
+            elif d > INTERVAL["write"]:
+                if state == STATE["write"]:
+                    tweet("レポートを書く手が止まったゾイ", api, me)
+                    state = STATE["stop"]
+
+            elif d <= INTERVAL["write"]:
+                if state == STATE["sleep"]:
+                    tweet("起きたゾイ", api, me)
+                    state = STATE["write"]
+                    sleep_cnt = 2
+                elif state == STATE["stop"]:
+                    tweet("レポートを再び書き始めたゾイ", api, me)
+                    state = STATE["write"]
+            prev = cur
+                
+            time.sleep(1)
+
+        except KeyboardInterrupt:
+            sys.exit(1)
